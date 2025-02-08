060381d83f2ba8ff22933bbea312a0d5b3b84269
AnnaLiulkova <al644722@gmail.com>
2025-02-09 01:38:04 +0200

    Перший коміт

---
+++ 2_Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Solution
+
+{
+    public static void Main(string[] args)
+    {
+        int n=int.Parse(Console.ReadLine());
+        int[] a=new int[n.ToString().Length];
+        int count=0;
+        if(0<=n && n<=2*Math.Pow(10,9))
+        {
+            for(int i=0; i<a.Length; i++)
+            {
+                a[i]=n%10;
+                n /= 10;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ EOlymp_VeryEasy/1_Simple_problem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int two_digit = int.Parse(Console.ReadLine());
+        if(two_digit>=10 && two_digit<=99)
+        {
+            int first_d = two_digit / 10;
+            int second_d = two_digit % 10;
+            Console.WriteLine($"{first_d} {second_d}");
+        }
+    }
+}
